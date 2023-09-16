16b297ee34964669b2c5a731151f7b04e09b7d81
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-16 19:34:10 +0500

    Message

--- Q10.js
+++
@@ -1,9 +0,0 @@
-// Question No.10
-// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-var names = ["Adnan Khalil", "Asif Riaz", "Zeeshan Ghani", "Amanual James", "Zeeshan Shafi"];
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
-console.log(names[4]);
-export {};
--- Q8.js
+++
@@ -1,5 +0,0 @@
-// Question No.8
-// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-var favouriteNumber = 804;
-console.log("My favourite number is", favouriteNumber);
-export {};
